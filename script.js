e49b3024a27866465e1a1b4e2f076adcdb392613
Narutzuna <gibraneduardo523@gmail.com>
2023-03-03 21:50:55 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const btn = document.querySelector ('[data-form-btn]');
 
+// Arrow function o funciones anonimas
 const createTask = (evento) => {
     // Parte para ingresar el valor de entrada
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]') ;
+    const task = document.createElement('li');
+    task.classList.add('card')
+    input.value = '';
+    // backticks
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+    list.appendChild(task);
+    console.log(content);
 };
-
 
 console.log(btn);
 
 // Listener
-
-// Arrow function o funciones anonimas
 btn.addEventListener('click', createTask);
 
 
