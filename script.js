2f76bbd468d6f6d0c4d5881fbea79ec492ad1337
Narutzuna <gibraneduardo523@gmail.com>
2023-03-05 00:51:25 -0600

    curso de reack aunque aun no se pude seleccionar

--- script.js
+++ script.js
@@ -11,12 +11,17 @@ const createTask = (evento) => {
     task.classList.add('card')
     input.value = '';
     // backticks
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
+    console.log(checkComplete)
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value
+    taskContent.appendChild(titleTask);
+    const content = `
   <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
     console.log(content);
 };
@@ -25,5 +30,13 @@ console.log(btn);
 
 // Listener
 btn.addEventListener('click', createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add('far');
+    i.classList.add('fa-check-square');
+    i.classList.add('icon');
+    return i;
+}
 
 
